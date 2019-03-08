824dc9e78b9efc18e5bbfaa31ea57f8235908c85
Ariel Webb <arielwebb12@gmail.com>
2019-03-08 11:53:18 -0500

    first commit

---
+++ src/main/java/org/wecancodeit/bloodypopcorn/BloodyPopcornApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.bloodypopcorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodyPopcornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodyPopcornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/bloodypopcorn/BloodyPopcornApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.bloodypopcorn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BloodyPopcornApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
