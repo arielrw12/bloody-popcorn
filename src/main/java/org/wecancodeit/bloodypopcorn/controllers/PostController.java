5a079014eeb5b68de1e306dd423df07b9ca621b4
Ariel Webb <arielwebb12@gmail.com>
2019-03-09 21:23:47 -0500

    main page styling

--- src/main/java/org/wecancodeit/bloodypopcorn/controllers/PostController.java
+++ src/main/java/org/wecancodeit/bloodypopcorn/controllers/PostController.java
@@ -5,6 +5,12 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.wecancodeit.bloodypopcorn.models.Author;
+import org.wecancodeit.bloodypopcorn.models.Genre;
+import org.wecancodeit.bloodypopcorn.models.Post;
+import org.wecancodeit.bloodypopcorn.models.Tag;
 import org.wecancodeit.bloodypopcorn.repositories.AuthorRepository;
 import org.wecancodeit.bloodypopcorn.repositories.GenreRepository;
 import org.wecancodeit.bloodypopcorn.repositories.PostRepository;
@@ -30,4 +36,30 @@ public class PostController {
 		model.addAttribute("post", postRepo.findAll());
 		return "post/allPosts";
 	}
+	
+	@PostMapping("/post/allPosts")
+	public String addPost(
+		@PathVariable Long id,
+		// ("post")
+		// may need to add above post line, trying to use it to fix
+		// Missing URI template variable 'post' for method parameter of type Long error
+		String title,
+		String body,
+		Genre genre,
+		Tag tag,
+		Author author
+		) {
+		Tag tag1 = tagRepo.findById(id).get();
+		Author author1 = authorRepo.findById(id).get();
+		postRepo.save(new Post(title, body, genre, tag1, author1));
+		return "redirect:/post/allPosts"; 
+	}
+	
+	@GetMapping("/post/{postId}")
+	public String getPostById(@PathVariable Long postId, Model model) {
+		model.addAttribute("post", postRepo.findById(postId).get());
+		return "post/individualPost";
+	}
+			
+			
 }
